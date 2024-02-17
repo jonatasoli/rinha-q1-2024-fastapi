e6cc3e2f56d8c4a32c8afc7b607b09f16fcf6e43
Jonatas Oliveira <contact@jonatasoliveira.dev>
2024-02-17 10:53:35 +0100

    feat: version with sync

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from datetime import datetime
-from fastapi import APIRouter, FastAPI, status
-from pydantic import BaseModel
+from typing import List
+from fastapi import APIRouter, FastAPI, status, HTTPException, Depends
+from pydantic import parse_obj_as
+from pydantic import BaseModel, Field, ConfigDict
+from sqlalchemy import Engine, create_engine
+from sqlalchemy.orm import sessionmaker
+from src.models import Clients, Transactions
+from sqlalchemy.orm import Session
+from sqlalchemy import and_, func, select
+
 
 main = APIRouter()
 
@@ -14,6 +22,20 @@ def create_app() -> FastAPI:
     )
     app.mount('/', main)
     return app
+
+sqlalchemy_database_url = 'postgresql://rinha:rinha@localhost:5432/rinha'
+
+engine = create_engine(
+        sqlalchemy_database_url,
+        pool_size=10,
+        max_overflow=1,
+)
+
+
+def get_session() -> Session:
+    with Session(bind=engine) as session:
+        yield session
+
 
 
 class Transaction(BaseModel):
@@ -37,7 +59,9 @@ class LastTransaction(BaseModel):
     valor: int
     tipo: str
     descricao: str
-    realizada_em: datetime
+    realizado_em: datetime
+    model_config = ConfigDict(from_attributes=True)
+
 
 
 class ExtractResponse(BaseModel):
@@ -46,34 +70,53 @@ class ExtractResponse(BaseModel):
 
 
 @main.post('/clientes/{id}/transacoes', status_code=status.HTTP_200_OK)
-def transaction(id: int, transaction: Transaction) -> TransactionResponse:
-    return TransactionResponse(limite=100000, saldo=-9098)
+def transaction(id: int, transaction: Transaction, db: Session = Depends(get_session)):
+    db_client = db.scalar(select(Clients).where(Clients.id == id))
+    if not db_client:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Client not exist'
+        )
+    if transaction.tipo.lower() == 'c':
+        db_client.saldo = db_client.saldo + transaction.valor
+    elif transaction.tipo.lower() == 'd':
+        _new_balance = db_client.saldo - transaction.valor
+        if _new_balance < db_client.limite:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail='transaction above limit!'
+            )
+        db_client.saldo = _new_balance
+    else:
+        raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail='transaction type not permitted!'
+        )
+    db_transaction = Transactions(
+        client_id = db_client.id,
+        valor = transaction.valor,
+        descricao=transaction.descricao,
+        tipo=transaction.tipo.lower(),
+    )
+    db.add_all([db_transaction, db_client])
+    db.commit()
+        
+    return TransactionResponse(limite=db_client.limite, saldo=db_client.saldo)
 
 
 @main.get('/clientes/{id}/extrato', status_code=status.HTTP_200_OK)
-def extract(id: int) -> ExtractResponse:
+def extract(id: int, db: Session = Depends(get_session)):
+    db_client = db.scalar(select(Clients).where(Clients.id == id))
+    if not db_client:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Client not exist'
+        )
+    _balance = Balance(
+        total=db_client.saldo,
+        limite=db_client.limite,
+        data_extrato=datetime.now()
+    )
+
+    db_transactions = db.scalars(select(Transactions).where(Transactions.client_id==id)).all()
+    transactions =  parse_obj_as(List[LastTransaction], db_transactions)
     return ExtractResponse(
-        saldo=Balance(
-            total=-9098,
-            data_extrato=datetime.fromisoformat('2024-01-17T02:34:41.217753Z'),
-            limite=100000,
-        ),
-        ultimas_transacoes=[
-            LastTransaction(
-                valor=10,
-                tipo='c',
-                descricao='descricao',
-                realizada_em=datetime.fromisoformat(
-                    '2024-01-17T02:34:38.543030Z'
-                ),
-            ),
-            LastTransaction(
-                valor=90000,
-                tipo='d',
-                descricao='descricao',
-                realizada_em=datetime.fromisoformat(
-                    '2024-01-17T02:34:38.543030Z'
-                ),
-            ),
-        ],
+        saldo=_balance,
+        ultimas_transacoes=transactions
     )
---
+++ src/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Clients(Base):
+    __tablename__ = 'clients'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    limite: Mapped[int] = mapped_column(default=0)
+    saldo: Mapped[int] = mapped_column(default=0)
+    criado_em: Mapped[datetime] = mapped_column(default=datetime.now())
+
+
+
+class Transactions(Base):
+    
+    __tablename__ = 'transactions'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    client_id: Mapped[int] = mapped_column(ForeignKey('clients.id'))
+    valor: Mapped[int]
+    descricao = mapped_column(String(10))
+    tipo = mapped_column(String(1))
+    realizado_em: Mapped[datetime] = mapped_column(default=datetime.now())
+
+
